7718611b04f0a1157b63881e8170579d9254713a
Edu Calleja <e.calleja.garcia@gmail.com>
2018-10-11 08:50:16 +1300

    Adds l3mapgen for NZ to pipeline

--- processing/03_l3_smi.sh
+++ processing/03_l3_smi.sh
@@ -1,16 +1,24 @@
 # Creates an SMI Map file from L3 bin files
 
+
 cd images/L3/A20032018_3MC_NSST
+# Full Earth
 l3mapgen ifile=A20032018.L3b_3MC_NSST ofile=A20032018.L3m_3MC_NSST product=sst interp=area north=90 south=-90 west=-180 east=180
+# NZ Only
+l3mapgen ifile=A20032018.L3b_3MC_NSST ofile=A20032018.L3m_3MC_NSST_NZ product=sst interp=area north=-34 south=-49 west=162.5 east=180
 
 cd ../A2018JAN_3MO_NSST
 l3mapgen ifile=A2018JAN_3MO_NSST ofile=A2018JAN.L3m_3MO_NSST product=sst interp=area north=90 south=-90 west=-180 east=180
+l3mapgen ifile=A2018JAN_3MO_NSST ofile=A2018JAN.L3m_3MO_NSST_NZ product=sst interp=area north=-34 south=-49 west=162.5 east=180
 
 cd ../A2013JAN_3MO_NSST
 l3mapgen ifile=A2013JAN_3MO_NSST ofile=A2013JAN.L3m_3MO_NSST product=sst interp=area north=90 south=-90 west=-180 east=180
+l3mapgen ifile=A2013JAN_3MO_NSST ofile=A2013JAN.L3m_3MO_NSST_NZ product=sst interp=area north=-34 south=-49 west=162.5 east=180
 
 cd ../A2008JAN_3MO_NSST
 l3mapgen ifile=A2008JAN_3MO_NSST ofile=A2008JAN.L3m_3MO_NSST product=sst interp=area north=90 south=-90 west=-180 east=180
+l3mapgen ifile=A2008JAN_3MO_NSST ofile=A2008JAN.L3m_3MO_NSST_NZ product=sst interp=area north=-34 south=-49 west=162.5 east=180
 
 cd ../A2003JAN_3MO_NSST
 l3mapgen ifile=A2003JAN_3MO_NSST ofile=A2003JAN.L3m_3MO_NSST product=sst interp=area north=90 south=-90 west=-180 east=180
+l3mapgen ifile=A2003JAN_3MO_NSST ofile=A2003JAN.L3m_3MO_NSST_NZ product=sst interp=area north=-34 south=-49 west=162.5 east=180
