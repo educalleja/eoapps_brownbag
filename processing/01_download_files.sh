09d75198e2cda10e40f1f957f3eb5b0cead75bc3
Edu Calleja <e.calleja.garcia@gmail.com>
2018-10-12 19:39:21 +1300

    Simplifies download pipeline

--- processing/01_download_files.sh
+++ processing/01_download_files.sh
@@ -2,24 +2,16 @@
 
 set -e
 
+START_YEAR=2003
+END_YEAR=2018
+YEAR=$START_YEAR
+echo "https://oceandata.sci.gsfc.nasa.gov/cgi/getfile/A${START_YEAR}001${END_YEAR}031.L3b_MO_NSST.nc" > files.txt
+while [ $YEAR -le $END_YEAR ]
+do
+  echo "https://oceandata.sci.gsfc.nasa.gov/cgi/getfile/A${YEAR}001${YEAR}031.L3b_MO_NSST.nc" >> files.txt
+  YEAR=$(($YEAR+1))
+done
+
+mkdir -p images/L3/
 wget --content-disposition -i files.txt -P images/L3/
-
-mkdir -p images/L3/A20032018_3MO_NSST
-mkdir -p images/L3/A2003JAN_3MO_NSST
-mkdir -p images/L3/A2008JAN_3MO_NSST
-mkdir -p images/L3/A2013JAN_3MO_NSST
-mkdir -p images/L3/A2018JAN_3MO_NSST
-
-mv images/L3/A200[2-3]*201*.L3b_MC_NSST.nc images/L3/A20032018_3MC_NSST/
-mv images/L3/A200[2-3]*200[2-3]*.L3b_MO_NSST.nc images/L3/A2003JAN_3MO_NSST/
-mv images/L3/A200[7-8]*200[7-8]*.L3b_MO_NSST.nc images/L3/A2008JAN_3MO_NSST/
-mv images/L3/A201[2-3]*201[2-3]*.L3b_MO_NSST.nc images/L3/A2013JAN_3MO_NSST/
-mv images/L3/A201[7-8]*201[7-8]*.L3b_MO_NSST.nc images/L3/A2018JAN_3MO_NSST/
-
-# Preparing textfile with files to be processed with l3bin
-# Input file for l3bin
-ls -l images/L3/A20032018_3MC_NSST | grep .nc | awk {'print $10'} > images/L3/A20032018_3MC_NSST/files.txt
-ls -l images/L3/A2003JAN_3MO_NSST | grep .nc | awk {'print $10'} > images/L3/A2003JAN_3MO_NSST/files.txt
-ls -l images/L3/A2008JAN_3MO_NSST | grep .nc | awk {'print $10'} > images/L3/A2008JAN_3MO_NSST/files.txt
-ls -l images/L3/A2013JAN_3MO_NSST | grep .nc | awk {'print $10'} > images/L3/A2013JAN_3MO_NSST/files.txt
-ls -l images/L3/A2018JAN_3MO_NSST | grep .nc | awk {'print $10'} > images/L3/A2018JAN_3MO_NSST/files.txt
+ls -l images/L3/ | grep _NSST.nc | awk {'print $10'} > images/L3/files.txt
